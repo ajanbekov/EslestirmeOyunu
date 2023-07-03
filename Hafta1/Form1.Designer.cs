df500388cad4ce1096e372e27a0c5ca3e1de4da5
JANBEKOV <JANBEKOV@LAPTOP-9LLNTJQM>
2023-07-03 10:04:16 +0300

    Proje dosyası ekle.

---
+++ Hafta1/Form1.Designer.cs
@@ -0,0 +1,333 @@
+﻿namespace Hafta1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            label1 = new Label();
+            tableLayoutPanel1 = new TableLayoutPanel();
+            label17 = new Label();
+            label16 = new Label();
+            label15 = new Label();
+            label14 = new Label();
+            label13 = new Label();
+            label12 = new Label();
+            label11 = new Label();
+            label10 = new Label();
+            label9 = new Label();
+            label8 = new Label();
+            label7 = new Label();
+            label6 = new Label();
+            label5 = new Label();
+            label4 = new Label();
+            label3 = new Label();
+            label2 = new Label();
+            timer1 = new System.Windows.Forms.Timer(components);
+            tableLayoutPanel1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.ForeColor = SystemColors.AppWorkspace;
+            label1.Location = new Point(334, 103);
+            label1.Name = "label1";
+            label1.Size = new Size(50, 20);
+            label1.TabIndex = 0;
+            label1.Text = "label1";
+            // 
+            // tableLayoutPanel1
+            // 
+            tableLayoutPanel1.BackColor = Color.SpringGreen;
+            tableLayoutPanel1.CellBorderStyle = TableLayoutPanelCellBorderStyle.Inset;
+            tableLayoutPanel1.ColumnCount = 4;
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 25F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 25F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 25F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 25F));
+            tableLayoutPanel1.Controls.Add(label17, 3, 3);
+            tableLayoutPanel1.Controls.Add(label16, 2, 3);
+            tableLayoutPanel1.Controls.Add(label15, 1, 3);
+            tableLayoutPanel1.Controls.Add(label14, 0, 3);
+            tableLayoutPanel1.Controls.Add(label13, 3, 2);
+            tableLayoutPanel1.Controls.Add(label12, 2, 2);
+            tableLayoutPanel1.Controls.Add(label11, 1, 2);
+            tableLayoutPanel1.Controls.Add(label10, 0, 2);
+            tableLayoutPanel1.Controls.Add(label9, 3, 1);
+            tableLayoutPanel1.Controls.Add(label8, 2, 1);
+            tableLayoutPanel1.Controls.Add(label7, 1, 1);
+            tableLayoutPanel1.Controls.Add(label6, 0, 1);
+            tableLayoutPanel1.Controls.Add(label5, 3, 0);
+            tableLayoutPanel1.Controls.Add(label4, 2, 0);
+            tableLayoutPanel1.Controls.Add(label3, 1, 0);
+            tableLayoutPanel1.Controls.Add(label2, 0, 0);
+            tableLayoutPanel1.Dock = DockStyle.Fill;
+            tableLayoutPanel1.Location = new Point(0, 0);
+            tableLayoutPanel1.Name = "tableLayoutPanel1";
+            tableLayoutPanel1.RowCount = 4;
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 25F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 25F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 25F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 25F));
+            tableLayoutPanel1.Size = new Size(532, 503);
+            tableLayoutPanel1.TabIndex = 1;
+            // 
+            // label17
+            // 
+            label17.Dock = DockStyle.Fill;
+            label17.Font = new Font("Webdings", 48F, FontStyle.Bold, GraphicsUnit.Point);
+            label17.Location = new Point(401, 377);
+            label17.Name = "label17";
+            label17.Size = new Size(126, 124);
+            label17.TabIndex = 15;
+            label17.Text = "c";
+            label17.TextAlign = ContentAlignment.MiddleCenter;
+            label17.Click += label_click;
+            // 
+            // label16
+            // 
+            label16.Dock = DockStyle.Fill;
+            label16.Font = new Font("Webdings", 48F, FontStyle.Bold, GraphicsUnit.Point);
+            label16.Location = new Point(269, 377);
+            label16.Name = "label16";
+            label16.Size = new Size(124, 124);
+            label16.TabIndex = 14;
+            label16.Text = "c";
+            label16.TextAlign = ContentAlignment.MiddleCenter;
+            label16.Click += label_click;
+            // 
+            // label15
+            // 
+            label15.Dock = DockStyle.Fill;
+            label15.Font = new Font("Webdings", 48F, FontStyle.Bold, GraphicsUnit.Point);
+            label15.Location = new Point(137, 377);
+            label15.Name = "label15";
+            label15.Size = new Size(124, 124);
+            label15.TabIndex = 13;
+            label15.Text = "c";
+            label15.TextAlign = ContentAlignment.MiddleCenter;
+            label15.Click += label_click;
+            // 
+            // label14
+            // 
+            label14.Dock = DockStyle.Fill;
+            label14.Font = new Font("Webdings", 48F, FontStyle.Bold, GraphicsUnit.Point);
+            label14.Location = new Point(5, 377);
+            label14.Name = "label14";
+            label14.Size = new Size(124, 124);
+            label14.TabIndex = 12;
+            label14.Text = "c";
+            label14.TextAlign = ContentAlignment.MiddleCenter;
+            label14.Click += label_click;
+            // 
+            // label13
+            // 
+            label13.Dock = DockStyle.Fill;
+            label13.Font = new Font("Webdings", 48F, FontStyle.Bold, GraphicsUnit.Point);
+            label13.Location = new Point(401, 252);
+            label13.Name = "label13";
+            label13.Size = new Size(126, 123);
+            label13.TabIndex = 11;
+            label13.Text = "c";
+            label13.TextAlign = ContentAlignment.MiddleCenter;
+            label13.Click += label_click;
+            // 
+            // label12
+            // 
+            label12.Dock = DockStyle.Fill;
+            label12.Font = new Font("Webdings", 48F, FontStyle.Bold, GraphicsUnit.Point);
+            label12.Location = new Point(269, 252);
+            label12.Name = "label12";
+            label12.Size = new Size(124, 123);
+            label12.TabIndex = 10;
+            label12.Text = "c";
+            label12.TextAlign = ContentAlignment.MiddleCenter;
+            label12.Click += label_click;
+            // 
+            // label11
+            // 
+            label11.Dock = DockStyle.Fill;
+            label11.Font = new Font("Webdings", 48F, FontStyle.Bold, GraphicsUnit.Point);
+            label11.Location = new Point(137, 252);
+            label11.Name = "label11";
+            label11.Size = new Size(124, 123);
+            label11.TabIndex = 9;
+            label11.Text = "c";
+            label11.TextAlign = ContentAlignment.MiddleCenter;
+            label11.Click += label_click;
+            // 
+            // label10
+            // 
+            label10.Dock = DockStyle.Fill;
+            label10.Font = new Font("Webdings", 48F, FontStyle.Bold, GraphicsUnit.Point);
+            label10.Location = new Point(5, 252);
+            label10.Name = "label10";
+            label10.Size = new Size(124, 123);
+            label10.TabIndex = 8;
+            label10.Text = "c";
+            label10.TextAlign = ContentAlignment.MiddleCenter;
+            label10.Click += label_click;
+            // 
+            // label9
+            // 
+            label9.Dock = DockStyle.Fill;
+            label9.Font = new Font("Webdings", 48F, FontStyle.Bold, GraphicsUnit.Point);
+            label9.Location = new Point(401, 127);
+            label9.Name = "label9";
+            label9.Size = new Size(126, 123);
+            label9.TabIndex = 7;
+            label9.Text = "c";
+            label9.TextAlign = ContentAlignment.MiddleCenter;
+            label9.Click += label_click;
+            // 
+            // label8
+            // 
+            label8.Dock = DockStyle.Fill;
+            label8.Font = new Font("Webdings", 48F, FontStyle.Bold, GraphicsUnit.Point);
+            label8.Location = new Point(269, 127);
+            label8.Name = "label8";
+            label8.Size = new Size(124, 123);
+            label8.TabIndex = 6;
+            label8.Text = "c";
+            label8.TextAlign = ContentAlignment.MiddleCenter;
+            label8.Click += label_click;
+            // 
+            // label7
+            // 
+            label7.Dock = DockStyle.Fill;
+            label7.Font = new Font("Webdings", 48F, FontStyle.Bold, GraphicsUnit.Point);
+            label7.Location = new Point(137, 127);
+            label7.Name = "label7";
+            label7.Size = new Size(124, 123);
+            label7.TabIndex = 5;
+            label7.Text = "c";
+            label7.TextAlign = ContentAlignment.MiddleCenter;
+            label7.Click += label_click;
+            // 
+            // label6
+            // 
+            label6.Dock = DockStyle.Fill;
+            label6.Font = new Font("Webdings", 48F, FontStyle.Bold, GraphicsUnit.Point);
+            label6.Location = new Point(5, 127);
+            label6.Name = "label6";
+            label6.Size = new Size(124, 123);
+            label6.TabIndex = 4;
+            label6.Text = "c";
+            label6.TextAlign = ContentAlignment.MiddleCenter;
+            label6.Click += label_click;
+            // 
+            // label5
+            // 
+            label5.Dock = DockStyle.Fill;
+            label5.Font = new Font("Webdings", 48F, FontStyle.Bold, GraphicsUnit.Point);
+            label5.Location = new Point(401, 2);
+            label5.Name = "label5";
+            label5.Size = new Size(126, 123);
+            label5.TabIndex = 3;
+            label5.Text = "c";
+            label5.TextAlign = ContentAlignment.MiddleCenter;
+            label5.Click += label_click;
+            // 
+            // label4
+            // 
+            label4.Dock = DockStyle.Fill;
+            label4.Font = new Font("Webdings", 48F, FontStyle.Bold, GraphicsUnit.Point);
+            label4.Location = new Point(269, 2);
+            label4.Name = "label4";
+            label4.Size = new Size(124, 123);
+            label4.TabIndex = 2;
+            label4.Text = "c";
+            label4.TextAlign = ContentAlignment.MiddleCenter;
+            label4.Click += label_click;
+            // 
+            // label3
+            // 
+            label3.Dock = DockStyle.Fill;
+            label3.Font = new Font("Webdings", 48F, FontStyle.Bold, GraphicsUnit.Point);
+            label3.Location = new Point(137, 2);
+            label3.Name = "label3";
+            label3.Size = new Size(124, 123);
+            label3.TabIndex = 1;
+            label3.Text = "c";
+            label3.TextAlign = ContentAlignment.MiddleCenter;
+            label3.Click += label_click;
+            // 
+            // label2
+            // 
+            label2.Dock = DockStyle.Fill;
+            label2.Font = new Font("Webdings", 48F, FontStyle.Bold, GraphicsUnit.Point);
+            label2.Location = new Point(5, 2);
+            label2.Name = "label2";
+            label2.Size = new Size(124, 123);
+            label2.TabIndex = 0;
+            label2.Text = "c";
+            label2.TextAlign = ContentAlignment.MiddleCenter;
+            label2.Click += label_click;
+            // 
+            // timer1
+            // 
+            timer1.Interval = 750;
+            timer1.Tick += timer1_Tick;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(532, 503);
+            Controls.Add(tableLayoutPanel1);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Eşleştirme Oyunu";
+            tableLayoutPanel1.ResumeLayout(false);
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private TableLayoutPanel tableLayoutPanel1;
+        private Label label17;
+        private Label label16;
+        private Label label15;
+        private Label label14;
+        private Label label13;
+        private Label label12;
+        private Label label11;
+        private Label label10;
+        private Label label9;
+        private Label label8;
+        private Label label7;
+        private Label label6;
+        private Label label5;
+        private Label label4;
+        private Label label3;
+        private Label label2;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
